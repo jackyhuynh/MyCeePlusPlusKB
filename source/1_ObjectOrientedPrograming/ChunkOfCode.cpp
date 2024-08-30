3487c628f1fbcf1620664b2fcce6ab6269c04602
trucdev <jackyhuynh87@gmail.com>
2024-08-30 12:14:33 -0500

    update the code of chunk

--- source/1_ObjectOrientedPrograming/ChunkOfCode.cpp
+++ source/1_ObjectOrientedPrograming/ChunkOfCode.cpp
@@ -2,17 +2,38 @@
 
 using namespace std;
 
-void print_anything(int input){
-    cout << to_string(input);
+
+int sum(int inputsQuantity)
+{
+    int input = 0;
+    int sum = 0;
+
+    for (int i = 0; i < inputsQuantity; i++)
+    {
+        cout << "Type a number: ";
+        cin >> input;
+        sum = sum + input;
+    }
+
+    return sum;
 }
+
+int getUserinput(){
+    int input = 0;
+    cout << endl<< "Type a number:";
+    cin >> input;
+    return input;
+}
+
 
 int main()
 {
-    int input=0;
-    cout << "hello world" << endl;
-    cout << "Type a number:";
-    cin >> input;
+    int sum = 0;
 
-    print_anything(input);
+    for (int i=0; i<3; i++ ){
+        sum += getUserinput();
+    }
+
+    cout << "sum is: " <<sum;
     return 0;
 }
