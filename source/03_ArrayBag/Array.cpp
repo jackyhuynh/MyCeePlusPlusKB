17fd6fa5a775993c777c2a36fc24222069418951
trucdev <jackyhuynh87@gmail.com>
2024-09-04 13:04:59 -0500

    add the normal array to the code space

---
+++ source/03_ArrayBag/Array.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string cars[]={"Covette", "Camry", "Camero"};
+    cout << "This is the car array: " << *cars << endl;
+    cout << "This is the car array: " << cars << endl;
+
+    for (int i = 0; i<3; i++)
+    {
+        cout << cars[i] << endl;
+    }
+
+    // This is another way to access without for
+    // cout << cars[3] << endl;
+    cout << cars[2] << endl;
+
+    bool index = true;
+
+    while (index){
+        cout << "It is always true" << endl;
+        index = false;
+        cout << "It is not always true anymore" << endl; 
+    }
+}
