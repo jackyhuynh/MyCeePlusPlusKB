54da9a94093f2359e7980b150fe74e5a7521f5ac
trucdev <jackyhuynh87@gmail.com>
2024-10-09 18:53:12 -0500

    add more functions for the sorted linked list

--- source/DataStructure/12_SortedList/SortedLinkedList.cpp
+++ source/DataStructure/12_SortedList/SortedLinkedList.cpp
@@ -35,6 +35,83 @@ public:
             current->next = newNode;
         }
     }
+    
+    bool deleteByValue(int value) {
+        if (head == nullptr) {
+            return false;  // List is empty
+        }
+
+        // Special case: the node to delete is the head
+        if (head->data == value) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            return true;
+        }
+
+        // Traverse the list to find the node to delete
+        Node* current = head;
+        while (current->next != nullptr && current->next->data != value) {
+            current = current->next;
+        }
+
+        // If we found the node with the value
+        if (current->next != nullptr && current->next->data == value) {
+            Node* nodeToDelete = current->next;
+            current->next = current->next->next;
+            delete nodeToDelete;
+            return true;
+        }
+
+        return false;  // Value not found
+    }
+
+    bool deleteByPosition(int position) {
+        if (position < 1 || head == nullptr) {
+            return false;  // Invalid position or empty list
+        }
+
+        // Special case: deleting the head
+        if (position == 1) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            return true;
+        }
+
+        // Traverse to the node just before the position
+        Node* current = head;
+        for (int i = 1; i < position - 1 && current->next != nullptr; i++) {
+            current = current->next;
+        }
+
+        // If we reached the position and there is a node to delete
+        if (current->next != nullptr) {
+            Node* nodeToDelete = current->next;
+            current->next = current->next->next;
+            delete nodeToDelete;
+            return true;
+        }
+
+        return false;  // Position is out of bounds
+    }
+
+
+    bool update(int position, int value) {
+        if (position < 1) {  // Invalid position check
+            return false;
+        }
+
+        Node* temp = head;
+        for (int i = 1; i <= position && temp != nullptr; i++) {
+            if (i == position) {
+                temp->data = value;
+                return true;
+            }
+            temp = temp->next;
+        }
+        return false;  // Return false if position is out of bounds
+    }
 
     void printList() const {
         Node* current = head;
