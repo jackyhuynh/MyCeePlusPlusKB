b3e06a376a5252742c3ddb449e7137add84729a0
trucdev <jackyhuynh87@gmail.com>
2024-11-18 21:24:21 -0600

    add in class content for heap

--- DataStructure/17_Heap/ch17_heap_array.cpp
+++ DataStructure/17_Heap/ch17_heap_array.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 /*
 * - Heap? 
-* - Max Heap, Min Heap
+* - Max Heap, Min Heap?
 * - Heap Sort?
-* - Reference Only: https://www.codingdrills.com/tutorial/heap-data-structure/introduction-to-heap-data-structure
-* - https://www.youtube.com/watch?v=Dvq-YKeuO9Y 
+* - Reference Only: 
+*   https://www.codingdrills.com/tutorial/heap-data-structure/introduction-to-heap-data-structure
+*   https://www.youtube.com/watch?v=Dvq-YKeuO9Y 
 */
 // Function to swap two elements
 void swap(int &first, int &second) {
